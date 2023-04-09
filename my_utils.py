9e5f5edccc0d47c3df706bbbda539c072f1900f6
liujing04 <129054828+liujing04@users.noreply.github.com>
2023-04-09 16:05:04 +0000

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -5,6 +5,7 @@ def load_audio(file,sr):
         # https://github.com/openai/whisper/blob/main/whisper/audio.py#L26
         # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
         # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        file=file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")#防止小白拷路径头尾带了空格和"和回车
         out, _ = (
             ffmpeg.input(file, threads=0)
             .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
