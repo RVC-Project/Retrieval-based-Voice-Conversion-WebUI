51bf09009b990a8c3b5a0bbd21b6569b94dc6b48
nadare <nadaretakeshi@yahoo.co.jp>
2023-04-12 12:05:16 +0000

    quality improvement using alpha query expansion.

--- infer/train-index.py
+++ infer/train-index.py
@@ -11,7 +11,32 @@ for name in sorted(list(os.listdir(inp_root))):
     npys.append(phone)
 big_npy=np.concatenate(npys,0)
 print(big_npy.shape)#(6196072, 192)#fp32#4.43G
-np.save("infer/big_src_feature_mi.npy",big_npy)
+
+num_expand = 16  # this is tunable params
+batch_size = 1024  # this is tunable params
+
+index = faiss.index_factory(256, "IVF512,PQ128x4fs,RFlat")
+original_norm = np.maximum(np.linalg.norm(big_npy, ord=2, axis=1, keepdims=True), 1e-9)
+big_npy /= original_norm
+index.train(big_npy)
+index.add(big_npy)
+dist, neighbor = index.search(big_npy, num_expand)
+
+expand_arrays = []
+ixs = np.arange(big_npy.shape[0])
+for i in range(-(-big_npy.shape[0]//batch_size)):
+    ix = ixs[i*batch_size:(i+1)*batch_size]
+    weight = np.power(np.einsum("nd,nmd->nm", big_npy[ix], big_npy[neighbor[ix]]), 3.)
+    expand_arrays.append(np.sum(big_npy[neighbor[ix]] * np.expand_dims(weight, axis=2),axis=1))
+big_npy = np.concatenate(expand_arrays, axis=0)
+
+# normalize index version
+big_npy = big_npy / np.maximum(np.linalg.norm(big_npy, ord=2, axis=1, keepdims=True), 1e-9)
+np.save("infer/big_src_feature_mi.npy", big_npy * original_norm)
+
+# not normalize index version
+# big_npy = big_npy / np.maximum(np.linalg.norm(big_npy, ord=2, axis=1, keepdims=True), 1e-9) * original_norm
+# np.save("infer/big_src_feature_mi.npy", big_npy)
 
 ##################train+add
 # big_npy=np.load("/bili-coeus/jupyter/jupyterhub-liujing04/vits_ch/inference_f0/big_src_feature_mi.npy")
