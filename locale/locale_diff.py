57fc9641b92dcf983b7ed45600462bf5a862f90f
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-04-15 21:17:32 +0800

    Update locale_diff.py

--- locale/locale_diff.py
+++ locale/locale_diff.py
@@ -32,7 +32,8 @@ for lang_file in languages:
 
     # Sort the keys of the language file to match the order of the standard file
     lang_data = OrderedDict(
-        sorted(lang_data.items(), key=lambda x: list(standard_data.keys()).index(x[0])))
+        sorted(lang_data.items(), key=lambda x: list(standard_data.keys()).index(x[0]))
+    )
 
     # Save the updated language file
     with open(lang_file, "w", encoding="utf-8") as f:
