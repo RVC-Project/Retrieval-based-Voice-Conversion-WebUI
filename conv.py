8d6697788b4a1b41105f2878395c8d253ec7db12
Zzzyt <Zzzyt205@gmail.com>
2023-05-19 18:22:31 +0800

    no more profiler & change default audio

--- conv.py
+++ conv.py
@@ -1,9 +1,12 @@
 import os
 
-os.chdir('logs/ai-zzzyt/')
-for i in os.listdir():
-    if i.endswith('.pt.trace.json'):
-        s=open(i,encoding='utf-8').read()
-        s=s.replace('\\','/')
-        open(i,'w',encoding='utf-8').write(s)
-                  
+# os.chdir('logs/ai-zzzyt/')
+def proc(d):
+    print('CONV:',d)
+    for i in os.listdir(d):
+        if i.endswith('.pt.trace.json'):
+            s=open(i,encoding='utf-8').read()
+            s=s.replace('\\','/')
+            open(i,'w',encoding='utf-8').write(s)
+proc('.')
+proc('logs/ai-zzzyt')
