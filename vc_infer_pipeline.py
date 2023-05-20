e6a8e440a2f55fec53ff3a2a0c03c09c393ed6e8
tzshao <66196038+shaotz@users.noreply.github.com>
2023-05-20 21:10:27 +0800

    Update vc_infer_pipeline.py
    1. Line 128, f0_coarse = np.rint(f0_mel).astype(np.int) is now:
    f0_coarse = np.rint(f0_mel).astype(np.int64)

--- vc_infer_pipeline.py
+++ vc_infer_pipeline.py
@@ -125,7 +125,7 @@ class VC(object):
         ) + 1
         f0_mel[f0_mel <= 1] = 1
         f0_mel[f0_mel > 255] = 255
-        f0_coarse = np.rint(f0_mel).astype(np.int)
+        f0_coarse = np.rint(f0_mel).astype(np.int64)
         return f0_coarse, f0bak  # 1-0
 
     def vc(
