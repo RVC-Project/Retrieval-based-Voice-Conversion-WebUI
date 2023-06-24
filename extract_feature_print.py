ccba65151e09b4f14795f69b0abf8132ee04d037
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-06-24 16:36:15 +0800

    优化代码结构

--- extract_feature_print.py
+++ extract_feature_print.py
@@ -19,14 +19,11 @@ import soundfile as sf
 import numpy as np
 from fairseq import checkpoint_utils
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
 elif torch.backends.mps.is_available():
     device = "mps"
-else:
-    device = "cpu"
 
 f = open("%s/extract_f0_feature.log" % exp_dir, "a+")
 
