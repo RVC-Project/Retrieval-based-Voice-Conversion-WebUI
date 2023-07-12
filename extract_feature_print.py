bd533199819dc24810f049ab62f57e400ff9288c
Naozumi <naozumi0512@gmail.com>
2023-07-12 16:34:26 +0800

    Update extract_feature_print.py

--- extract_feature_print.py
+++ extract_feature_print.py
@@ -1,6 +1,7 @@
 import os, sys, traceback
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
 
 # device=sys.argv[1]
 n_part = int(sys.argv[2])
