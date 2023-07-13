5131da557a196ed06f7963728758826f73f490ab
Zzzyt <Zzzyt205@gmail.com>
2023-07-13 20:27:46 +0800

    fluid container

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
-python infer-web.py
+@REM set CUDA_VISIBLE_DEVICES=""
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
+python infer-web.py --port 7897
