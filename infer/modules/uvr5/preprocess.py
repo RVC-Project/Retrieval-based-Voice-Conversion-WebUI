cd924f9eec371e7bb1ade8e7b41fc9dfdf24f557
Ftps <ftpsflandre@gmail.com>
2023-08-20 13:43:39 +0900

    fix uvr5 path

--- infer/modules/uvr5/preprocess.py
+++ infer/modules/uvr5/preprocess.py
@@ -26,7 +26,7 @@ class AudioPre:
             "agg": agg,
             "high_end_process": "mirroring",
         }
-        mp = ModelParameters("lib/uvr5_pack/lib_v5/modelparams/4band_v2.json")
+        mp = ModelParameters("infer/lib/uvr5_pack/lib_v5/modelparams/4band_v2.json")
         model = Nets.CascadedASPPNet(mp.param["bins"] * 2)
         cpk = torch.load(model_path, map_location="cpu")
         model.load_state_dict(cpk)
