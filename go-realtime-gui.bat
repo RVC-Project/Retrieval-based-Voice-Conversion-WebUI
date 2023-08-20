c1052fe4c57494166916c07a8524a93dee110e2f
Azure Crab <azure_crab@gmail.com>
2023-08-20 18:53:42 +0800

    增加注释

--- go-realtime-gui.bat
+++ go-realtime-gui.bat
@@ -1,2 +1,12 @@
-runtime\python.exe gui_v1.py
+@echo off
+
+@REM 定义你使用的python，指向你配好的环境中的python路径
+@REM     如 C:\Users\yourname\miniconda3\envs\rvc\python.exe
+set PYTHON=
+
+@REM 默认配置
+if not defined PYTHON (set PYTHON=python)
+
+@REM 实际执行
+%PYTHON% gui_v1.py
 pause
