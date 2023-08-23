b4c10ce37d8799930d53de1475b42582f0857a6b
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2023-08-23 18:52:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -168,10 +168,11 @@ class Config:
                     )
                 except:
                     pass
-            import torch_directml
-
-            self.device = torch_directml.device(torch_directml.default_device())
-            self.is_half = False
+            if(self.device!="cpu"):
+                import torch_directml
+    
+                self.device = torch_directml.device(torch_directml.default_device())
+                self.is_half = False
         else:
             if self.instead:
                 print(f"use {self.instead} instead")
