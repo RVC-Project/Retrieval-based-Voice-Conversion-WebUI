740625fd2b385b5633efeff7e64874f29c41902f
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-08-28 21:58:36 +0900

    fix i18n

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -15,7 +15,7 @@ class I18nAuto:
             language = locale.getdefaultlocale()[
                 0
             ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./lib/i18n/{language}.json"):
+        if not os.path.exists(f"./i18n/locale/{language}.json"):
             language = "en_US"
         self.language = language
         # print("Use Language:", language)
