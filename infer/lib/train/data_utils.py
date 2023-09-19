36456e3908b63de57fa7670dd6db7f731dc13970
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-09-19 21:15:30 +0900

    replace warn (#1255)

--- infer/lib/train/data_utils.py
+++ infer/lib/train/data_utils.py
@@ -113,7 +113,7 @@ class TextAudioLoaderMultiNSFsid(torch.utils.data.Dataset):
             try:
                 spec = torch.load(spec_filename)
             except:
-                logger.warn("%s %s", spec_filename, traceback.format_exc())
+                logger.warning("%s %s", spec_filename, traceback.format_exc())
                 spec = spectrogram_torch(
                     audio_norm,
                     self.filter_length,
@@ -305,7 +305,7 @@ class TextAudioLoader(torch.utils.data.Dataset):
             try:
                 spec = torch.load(spec_filename)
             except:
-                logger.warn("%s %s", spec_filename, traceback.format_exc())
+                logger.warning("%s %s", spec_filename, traceback.format_exc())
                 spec = spectrogram_torch(
                     audio_norm,
                     self.filter_length,
--- infer/modules/train/train.py
+++ infer/modules/train/train.py
@@ -99,7 +99,7 @@ def main():
         n_gpus = 1
     if n_gpus < 1:
         # patch to unblock people without gpus. there is probably a better way.
-        logger.warn("NO GPU DETECTED: falling back to CPU - this may take a while")
+        logger.warning("NO GPU DETECTED: falling back to CPU - this may take a while")
         n_gpus = 1
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = str(randint(20000, 55555))
