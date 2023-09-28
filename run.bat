5cc689b5c316efec5948710de30ba8bc23580575
Zzzyt <Zzzyt205@gmail.com>
2023-09-28 23:42:26 +0800

    only load rmvpe when necessary

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @REM set CUDA_VISIBLE_DEVICES=""
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
-python infer-web.py --port 7897
+".venv/Scripts/python.exe" infer-web.py --port 7897
