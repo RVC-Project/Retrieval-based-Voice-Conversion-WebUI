ec8107cf5d84088b39afe108dbef986f3c74139e
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-20 22:00:02 +0100

    remove unused prints

--- infer/lib/train/mel_processing.py
+++ infer/lib/train/mel_processing.py
@@ -51,12 +51,6 @@ def spectrogram_torch(y, n_fft, sampling_rate, hop_size, win_size, center=False)
     Returns:
         :: (B, Freq, Frame) - Linear-frequency Linear-amplitude spectrogram
     """
-
-    # Validation
-    if torch.min(y) < -1.07:
-        logger.debug("min value is %s", str(torch.min(y)))
-    if torch.max(y) > 1.07:
-        logger.debug("max value is %s", str(torch.max(y)))
     
     # Window - Cache if needed
     global hann_window
