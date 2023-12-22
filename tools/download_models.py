d2a9530be9240abdd3ea1beca5c6cf86a12d93d2
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-22 17:09:16 +0100

    better downloader

--- tools/download_models.py
+++ tools/download_models.py
@@ -5,9 +5,28 @@ import requests
 RVC_DOWNLOAD_LINK = "https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+ASSETS_DIR = BASE_DIR / "assets"
+PRETRAINED_DIR = ASSETS_DIR / "pretrained"
+PRETRAINED_V2_DIR = ASSETS_DIR / "pretrained_v2"
+UVR5_WEIGHTS_DIR = ASSETS_DIR / "uvr5_weights"
+
+MODEL_NAMES = [
+    "D32k.pth",
+    "D40k.pth",
+    "D48k.pth",
+    "G32k.pth",
+    "G40k.pth",
+    "G48k.pth",
+    "f0D32k.pth",
+    "f0D40k.pth",
+    "f0D48k.pth",
+    "f0G32k.pth",
+    "f0G40k.pth",
+    "f0G48k.pth",
+]
 
 
-def dl_model(link, model_name, dir_name):
+def download_model(link, model_name, dir_name):
     with requests.get(f"{link}{model_name}") as r:
         r.raise_for_status()
         os.makedirs(os.path.dirname(dir_name / model_name), exist_ok=True)
@@ -17,52 +36,31 @@ def dl_model(link, model_name, dir_name):
 
 
 if __name__ == "__main__":
-    print("Downloading hubert_base.pt...")
-    dl_model(RVC_DOWNLOAD_LINK, "hubert_base.pt", BASE_DIR / "assets/hubert")
-    print("Downloading rmvpe.pt...")
-    dl_model(RVC_DOWNLOAD_LINK, "rmvpe.pt", BASE_DIR / "assets/rmvpe")
-    print("Downloading vocals.onnx...")
-    dl_model(
+    print("Downloading models")
+
+    # Download hubert_base.pt, rmvpe.pt, rmvpe.onnx
+    for model in ["hubert_base.pt", "rmvpe.pt", "rmvpe.onnx"]:
+        print(f"Downloading {model}...")
+        download_model(RVC_DOWNLOAD_LINK, model, ASSETS_DIR / model.split(".")[0])
+
+    # Download pretrained models
+    for dir_name in [PRETRAINED_DIR, PRETRAINED_V2_DIR]:
+        print(f"\nDownloading {dir_name.name} models:")
+        for model in MODEL_NAMES:
+            print(f"Downloading {model}...")
+            download_model(RVC_DOWNLOAD_LINK + f"{dir_name.name}/", model, dir_name)
+
+    # Download vocals.onnx
+    print("Downloading vocals.onnx")
+    download_model(
         RVC_DOWNLOAD_LINK + "uvr5_weights/onnx_dereverb_By_FoxJoy/",
         "vocals.onnx",
-        BASE_DIR / "assets/uvr5_weights/onnx_dereverb_By_FoxJoy",
+        ASSETS_DIR / "uvr5_weights/onnx_dereverb_By_FoxJoy",
     )
 
-    rvc_models_dir = BASE_DIR / "assets/pretrained"
-
-    print("Downloading pretrained models:")
-
-    model_names = [
-        "D32k.pth",
-        "D40k.pth",
-        "D48k.pth",
-        "G32k.pth",
-        "G40k.pth",
-        "G48k.pth",
-        "f0D32k.pth",
-        "f0D40k.pth",
-        "f0D48k.pth",
-        "f0G32k.pth",
-        "f0G40k.pth",
-        "f0G48k.pth",
-    ]
-    for model in model_names:
-        print(f"Downloading {model}...")
-        dl_model(RVC_DOWNLOAD_LINK + "pretrained/", model, rvc_models_dir)
-
-    rvc_models_dir = BASE_DIR / "assets/pretrained_v2"
-
-    print("Downloading pretrained models v2:")
-
-    for model in model_names:
-        print(f"Downloading {model}...")
-        dl_model(RVC_DOWNLOAD_LINK + "pretrained_v2/", model, rvc_models_dir)
-
-    print("Downloading uvr5_weights:")
-
-    rvc_models_dir = BASE_DIR / "assets/uvr5_weights"
-
-    model_names = [
+    # Download uvr5_weights
+    print("\nDownloading uvr5_weights")
+    for model in [
         "HP2-%E4%BA%BA%E5%A3%B0vocals%2B%E9%9D%9E%E4%BA%BA%E5%A3%B0instrumentals.pth",
         "HP2_all_vocals.pth",
         "HP3_all_vocals.pth",
@@ -71,9 +69,8 @@ if __name__ == "__main__":
         "VR-DeEchoAggressive.pth",
         "VR-DeEchoDeReverb.pth",
         "VR-DeEchoNormal.pth",
-    ]
-    for model in model_names:
+    ]:
         print(f"Downloading {model}...")
-        dl_model(RVC_DOWNLOAD_LINK + "uvr5_weights/", model, rvc_models_dir)
+        download_model(RVC_DOWNLOAD_LINK + "uvr5_weights/", model, UVR5_WEIGHTS_DIR)
 
     print("All models downloaded!")
