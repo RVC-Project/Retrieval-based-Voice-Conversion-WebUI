8c8325cd54d48a23ed42a78e860b8ca0fbd2e0a2
nr1a <82102170+nr1a@users.noreply.github.com>
2024-01-17 17:47:09 +0900

    support L4

--- infer-web.py
+++ infer-web.py
@@ -93,6 +93,7 @@ if torch.cuda.is_available() or ngpu != 0:
                 "M4",
                 "T4",
                 "TITAN",
+                "L4",
             ]
         ):
             # A10#A100#V100#A40#P40#M40#K80#A4500
