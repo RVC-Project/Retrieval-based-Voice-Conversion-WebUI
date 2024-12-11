a2ff0b17dc7d7ca6c7e2dd608dfa222cb5032287
Blaise <blaise@applio.org>
2024-12-11 23:22:05 +0100

    Remove unused ResBlock check as we only use ResBlock 1

--- infer/lib/infer_pack/modules.py
+++ infer/lib/infer_pack/modules.py
@@ -249,9 +249,9 @@ class WN(torch.nn.Module):
         return self
 
 
-class ResBlock1(torch.nn.Module):
+class ResBlock(torch.nn.Module):
     def __init__(self, channels, kernel_size=3, dilation=(1, 3, 5)):
-        super(ResBlock1, self).__init__()
+        super(ResBlock, self).__init__()
         self.convs1 = nn.ModuleList(
             [
                 weight_norm(
@@ -355,62 +355,6 @@ class ResBlock1(torch.nn.Module):
                 ):
                     torch.nn.utils.remove_weight_norm(l)
         for l in self.convs2:
-            for hook in l._forward_pre_hooks.values():
-                if (
-                    hook.__module__ == "torch.nn.utils.weight_norm"
-                    and hook.__class__.__name__ == "WeightNorm"
-                ):
-                    torch.nn.utils.remove_weight_norm(l)
-        return self
-
-
-class ResBlock2(torch.nn.Module):
-    def __init__(self, channels, kernel_size=3, dilation=(1, 3)):
-        super(ResBlock2, self).__init__()
-        self.convs = nn.ModuleList(
-            [
-                weight_norm(
-                    Conv1d(
-                        channels,
-                        channels,
-                        kernel_size,
-                        1,
-                        dilation=dilation[0],
-                        padding=get_padding(kernel_size, dilation[0]),
-                    )
-                ),
-                weight_norm(
-                    Conv1d(
-                        channels,
-                        channels,
-                        kernel_size,
-                        1,
-                        dilation=dilation[1],
-                        padding=get_padding(kernel_size, dilation[1]),
-                    )
-                ),
-            ]
-        )
-        self.convs.apply(init_weights)
-        self.lrelu_slope = LRELU_SLOPE
-
-    def forward(self, x, x_mask: Optional[torch.Tensor] = None):
-        for c in self.convs:
-            xt = F.leaky_relu(x, self.lrelu_slope)
-            if x_mask is not None:
-                xt = xt * x_mask
-            xt = c(xt)
-            x = xt + x
-        if x_mask is not None:
-            x = x * x_mask
-        return x
-
-    def remove_weight_norm(self):
-        for l in self.convs:
-            remove_weight_norm(l)
-
-    def __prepare_scriptable__(self):
-        for l in self.convs:
             for hook in l._forward_pre_hooks.values():
                 if (
                     hook.__module__ == "torch.nn.utils.weight_norm"
