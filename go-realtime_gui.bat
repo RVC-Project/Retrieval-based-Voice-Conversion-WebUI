f748ba4c48ca3e9b5790e7d59707bd29ae5a6a3e
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2026-07-20 00:13:58 +0800

    Improve Windows startup scripts

--- go-realtime_gui.bat
+++ go-realtime_gui.bat
@@ -4,5 +4,6 @@ set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
 set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
+echo 首次启动程序可能需要耐心等待20秒
 runtime\python.exe -I realtime_gui.py
 pause
--- go-webui.bat
+++ go-webui.bat
@@ -4,5 +4,8 @@ set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
 set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
+set "GRADIO_ANALYTICS_ENABLED=False"
+set "NO_PROXY=localhost,127.0.0.1,::1,%NO_PROXY%"
+echo 首次启动程序可能需要耐心等待20秒
 runtime\python.exe -I webui.py --pycmd runtime\python.exe --port 7897
 pause
